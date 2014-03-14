0c569d7b35bc36e3df23bd3ca3658d16e1f4e168
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-14 14:35:38 -0700

    setup relationships, add commenting

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
 
+  # Create relationships
+  has_many :status_reports
+
   # Add validations
   validates_presence_of :name, :manager, :organization
 end
--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -1,5 +1,8 @@
 class StatusReport < ActiveRecord::Base
+  
+  # Create relationships
   belongs_to :project
   
+  # Add validations
   validates_presence_of :project_id, :summary, :overall
 end
