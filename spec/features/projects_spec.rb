9b49e02fb152f164f972b53d9296ae05db0eecb0
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-14 09:58:57 -0700

    write scenario for adding a new project

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -8,4 +8,40 @@ describe "Projects" do
       page.should have_content('projects')
     end
   end
+  
+  describe "GET /projects/new" do
+    it "Loads the new project form" do
+      visit '/projects/new'
+      page.should have_content("Add a New Project")
+    end
+  end
 end
+
+feature "Project Management" do
+  scenario "creating a new project" do
+    visit projects_path
+    click_link "New Project"
+    
+    fill_in "Name", with: "Test Project"
+    fill_in "Manager", with: "Jane Doe"
+    fill_in "Organization", with: "CWS"
+    click_button "Save"
+    
+    expect(page).to have_text("Test Project")
+  end
+end
+    
+
+
+## spec/features/widget_management_spec.rb
+#feature "widget management" do
+#  scenario "creating a new widget" do
+#    visit root_url
+#    click_link "New Widget"
+#
+#    fill_in "Name", with: "Awesome Widget"
+#    click_button "Create Widget"
+#
+#    expect(page).to have_text("Widget was successfully created.")
+#  end
+#end
