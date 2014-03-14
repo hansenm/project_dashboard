2ec67ccd909b9c7921850cbe05dc68aa3a9b87ae
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-14 08:57:50 -0700

    add validation tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe Project do
-  it "is valid with name, manager and organization"
-  it "is not valid with an empty name"
+
+  before(:each) do
+    @project = FactoryGirl.build(:project)
+  end
+
+  it "is valid with name, manager and organization" do
+    @project.should be_valid
+  end
+
+  it "is not valid with an empty name" do
+    @project.name = nil
+    @project.should_not be_valid
+  end
+  
+  it "is not valid with an empty manager" do
+    @project.manager = nil
+    @project.should_not be_valid
+  end
+  
+  it "is not valid with an empty organization" do
+    @project.organization = nil
+    @project.should_not be_valid
+  end
 end
