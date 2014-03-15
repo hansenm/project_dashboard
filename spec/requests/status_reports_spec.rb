d1c2fc331b8cf96847ed5a7903ed974e3d46e8e5
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-14 19:46:16 -0700

    add request tests for status reports

--- spec/requests/status_reports_spec.rb
+++ spec/requests/status_reports_spec.rb
@@ -1,11 +1,66 @@
 require 'spec_helper'
 
 describe "StatusReports" do
-  describe "GET /status_reports" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get status_reports_index_path
+
+  describe "Get /projects/status_reports/new" do
+    it "successfully loads a new status reports page" do
+      @project = FactoryGirl.build(:project)
+      @project.save
+      get new_project_status_report_path(@project)
       response.status.should be(200)
     end
   end
+
+  describe "POST /projects/status_reports" do
+    it "successfully creates a new status report" do
+      @project = FactoryGirl.build(:project)
+      @project.save
+      
+      post project_status_reports_path(@project.id), status_report: { summary: "Test", overall: "test"}
+      response.status.should be(302)
+    end
+    
+    it "doesn't redirect an invalid status report" do
+      @status_report = FactoryGirl.build(:status_report)
+      @status_report.save
+      post project_status_reports_path(@project.id), status_report: { summary: "", overall: "test"}
+      response.status.should be(200)
+    end
+  end
+
+  describe "Get /projects/status_reports/id/edit" do
+    it "successfully loads a new status reports page" do
+      @status_report = FactoryGirl.build(:status_report)
+      @status_report.save
+      get edit_project_status_report_path(@status_report.project, @status_report)
+      response.status.should be(200)
+    end
+  end
+  
+  describe "Put /projects/id/status_reports/id" do
+    it "successfully updates a status report" do
+      @status_report = FactoryGirl.build(:status_report)
+      @status_report.save
+      put project_status_report_path(@status_report.project, @status_report), status_report: { summary: "Test", overall: "test"}
+      response.status.should be(302)
+    end
+    
+    it "doesn't redirect an invalid status report" do
+      @status_report = FactoryGirl.build(:status_report)
+      @status_report.save
+      put project_status_report_path(@status_report.project, @status_report), status_report: { summary: "", overall: "test"}
+      response.status.should be(200)
+    end
+  end
+
+  describe "DELETE /projects/id/status_reports/id" do
+    it "successfully deletes a status report" do
+      @status_report = FactoryGirl.build(:status_report)
+      @status_report.save
+      
+      delete project_status_report_path(@status_report.project,@status_report)
+      response.status.should be(302)
+    end
+  end
+
 end
