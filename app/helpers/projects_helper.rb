42dec2371bbe0447182fa2e632e0e97e7e5e0904
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-30 01:51:11 -0700

    continue refactor for visual status indicator

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,14 +1,16 @@
 module ProjectsHelper
 
-  def overall_status_indicator(status)
+  def status_indicator(status)
     case status
     when "green"
-      return "alert alert-success"
+      status_class = "alert alert-success"
     when "yellow"
-      return "alert"
+      status_class = "alert"
     else
-      return "alert alert-error"
+      status_class = "alert alert-error"
     end
+    
+    return content_tag(:span, " ", class: "status " + status_class)
   end
 
 end
